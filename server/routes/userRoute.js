f5f8eea86232008d0ffb2d9d06cf41bf327c5def
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2023-11-22 10:19:02 +0530

    getAll,getOne,update,delete

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { create } from '../controller/userController.js';
+import {
+  create,
+  getAll,
+  getOne,
+  update,
+  userDelete,
+} from '../controller/userController.js';
 
 const route = express.Router();
 
 route.post('/create', create);
+route.get('/getall', getAll);
+route.get('/getone/:id', getOne);
+route.put('/update/:id', update);
+route.delete('/userdelete/:id', userDelete);
 
 export default route;
